ee35baf67e0197c29314490c793b5ab8b0b1031b
Manan Patel <mananpatel690@gmail.com>
2022-06-12 17:15:34 -0400

    sqrt(x)

---
+++ sqrt.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    long long mySqrt(int x) {
+        long long l = 0, r = x, ans, mid;
+        while (l <= r){
+            mid = l + (r-l)/2;
+            if (mid * mid == x) return mid;
+            if (mid * mid < x){
+                l = mid + 1;
+                ans = mid;
+            } else {
+                r = mid -1;
+            }
+        }
+        return ans;
+    }
+};
