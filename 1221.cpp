319af91a1501f93c0051e5bbbc32cebb4bfaa274
manan-dev <mananpatel6902@gmail.com>
2022-07-01 17:17:06 -0400

    Split a String in Balanced Strings

---
+++ 1221.cpp
@@ -0,0 +1,19 @@
+class Solution
+{
+public:
+   int balancedStringSplit(string s)
+   {
+      int ans = 0, cnt = 0;
+
+      for (int i = 0; i < s.size(); i++)
+      {
+         if (s[i] == 'L')
+            cnt++;
+         if (s[i] == 'R')
+            cnt--;
+         if (cnt == 0)
+            ans++;
+      }
+      return ans;
+   }
+};
